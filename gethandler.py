a3b692b9e2906c295ba850b78eb40c28aefc02cd
RupaliVyas <43618937+RupaliVyas@users.noreply.github.com>
2019-10-13 17:04:07 +0530

    Update gethandler.py

--- gethandler.py
+++ gethandler.py
@@ -1,3 +1,4 @@
+
 import tornado.web
 from customer import Customer
 import json
@@ -7,5 +8,5 @@ class GetHandler(tornado.web.RequestHandler):
     def initialize(self, custs):
         self.customers = custs
         
-    def get(self):
+    def get(self,contact):
         self.write(self.customers.json_list())
